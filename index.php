3ab7e6ce22da1c380f5903467683a90fd495d02f
Jonathan <jonathan.grangien@gmail.com>
2014-02-01 22:54:21 +0100

    Turned into php and took out header, footer
    what about closing of php tags?

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+	include "header.php"
+?>
+	<div id="content">
+		<div class="contentField">
+			<h1>Under konstruktion</h1>
+			<br/>
+			<img class="cloudBear" src="intropic.png" alt="bear" />
+		</div>
+	</div><!--End content-->
+
+	<div class="push"></div><!--push footer down-->
+</div> <!--End Wrapper-->
+
+<?php
+	include "footer.php"
+?>
