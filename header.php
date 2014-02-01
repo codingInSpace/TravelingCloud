b5d0b2f467e8145212b4fcb2c1f514dc8e6ab57e
Jonathan <jonathan.grangien@gmail.com>
2014-02-01 22:55:01 +0100

    Made independent from index file

---
+++ header.php
@@ -0,0 +1,13 @@
+<?php print("<!DOCTYPE html>"); ?>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>Traveling Cloud</title>
+	<link rel="stylesheet" type="style/css" href="style.css"/>
+</head>
+<body>
+<div class="wrapper">
+	<div class="header">
+		<div class="headerBar">
+		</div>
+	</div><!--End header-->
